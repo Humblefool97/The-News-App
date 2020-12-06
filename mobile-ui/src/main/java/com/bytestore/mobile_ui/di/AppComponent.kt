20825b344cea9c9a654f36a78840a4a28d6745b1
rajeevranganathan <Puttin)(*051>
2020-12-06 23:00:01 +0530

    Set up dagger and Appcomponent

---
+++ mobile-ui/src/main/java/com/bytestore/mobile_ui/di/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.bytestore.mobile_ui.di
+
+import android.app.Application
+import com.bytestore.mobile_ui.NewsApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AndroidInjectionModule::class])
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+        fun build(): AppComponent
+    }
+
+    fun inject(app: NewsApplication)
+}
